626fbf398b98d5798e875bb0b94d87f023106e6e
MarcoFriedrichs <marco.friedr@gmail.com>
2020-06-28 16:14:16 +0200

    commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero">
+    <img/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  data () {
+    return {
+      message: 'Try change me!'
+    }
+  }
+}
+</script>
+
+<style>
+    .hero {
+        background: rgb(223, 223, 223);
+        width: 100%;
+        height: 100vh;
+    }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Hero>Yo</Hero>
+</template>
+
+<script>
+import Hero from '../components/Hero'
+
+export default {
+  components: {
+    Hero
+  },
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>
